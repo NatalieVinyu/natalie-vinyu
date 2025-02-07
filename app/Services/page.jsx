9c28de571352545f611843d690bd3373a176e09e
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2025-02-07 20:34:34 +0200

    added service cards

--- app/Services/page.jsx
+++ app/Services/page.jsx
@@ -7,9 +7,31 @@ const Services = () => {
 
       <div className='flex align-center items-center'>
         <div className='max-w-sm rounded overflow-hidden shadow-lg bg-white m-4'>
-          <div className='px-6 py-4'>
-            <div className='font-bold text-xl mb-2'>
-              card
+
+          <div>
+            <div className='text-center font-bold text-2xl mb-2 px-6 py-4'>
+              Calender Management
+            </div>
+
+            <div>
+              <img src="/Calendar-management-800x440.webp" alt="Calender Management"/>
+            </div>
+
+            <div className='px-8 py-8'>
+              <ul className='list-disc list-inside'>
+                <li>
+                  Scheduling Appointments
+                </li>
+                <li>
+                  Managing Multiple Calenders
+                </li>
+                <li>
+                  Setting Reminders
+                </li>
+                <li>
+                  Organizing Meetings
+                </li>
+              </ul>
             </div>
 
           </div>
