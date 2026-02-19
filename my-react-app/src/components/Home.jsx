2a42c923415d30934635a89f78badba2f463a606
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-02-19 17:07:40 +0200

    Enhance Home component layout and styling

--- my-react-app/src/components/Home.jsx
+++ my-react-app/src/components/Home.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 function Home() {
   return (
     <div className='h-screen bg-fuchsia-50 flex flex-col justify-center items-center space-y-10'>
-      <div>
-        <h1 className='text-6xl font-bold'>Natalie Vinyu</h1>
-        <p className='text-3xl text-center'>Software Engineer</p>
+      <div className='space-y-8'>
+        <h1 className='text-6xl md:text-8xl font-bold'>Natalie Vinyu</h1>
+        <p className='text-3xl md:text-4xl text-center'>Software Engineer</p>
       </div>
 
-      <div className='gap-6'>
-        <button>Download CV</button>
-        <button>Let's Connect</button>
+      <div className='flex md:flex-row gap-6 mt-12'>
+        <button className='px-6 py-3 bg-fuchsia-200 rounded-lg cursor-pointer'>Download CV</button>
+        <button className='px-6 py-3 bg-fuchsia-100 rounded-lg cursor-pointer'>Let's Connect</button>
       </div>
     </div>
   )
