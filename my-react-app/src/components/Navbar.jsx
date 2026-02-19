73ab1b29583a2c1fc9bb4146f69a40a0ee620564
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-02-19 14:33:57 +0200

    Refactor Navbar component and integrate Tailwind CSS styles

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -2,8 +2,36 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-      Navbar
+     <div className='fixed w-full bg-gray-100 bg-transparent bg-opacity-25 backdrop-blur-md shadow-lg p-8 z-10 top-0'>
+      <nav className='container'>        
+          <ul className='flex space-x-10 justify-end'>
+            <li>
+              <a href='#home'>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href='#about'>
+                About
+              </a>
+            </li>
+            <li>
+              <a href='#skills'>
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href='#projects'>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href='#contact'>
+                Contact
+              </a>
+            </li>
+          </ul>
+      </nav>
     </div>
   )
 }
