eae9ebf3c79d19d5f83a286a70c7568aa91964f5
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-02-23 01:07:22 +0200

    Update Projects component with new project details and links

--- my-react-app/src/components/Projects.jsx
+++ my-react-app/src/components/Projects.jsx
@@ -11,58 +11,72 @@ function Projects() {
       title: "RiverView Spa",
       description: "A fully responsive spa website redesign built with Bootstrap to enhance usability, aesthetics, and cross-device performance.",
       skills: ["HTML", "CSS", "Bootstrap"],
+      codeLink: "https://github.com/NatalieVinyu/River-View-Spa",
+      demoLink: "https://riverviewspa.netlify.app/",
     },
     {
       image: project2,
       title: "Quiz Hub",
       description: "A team-developed quiz application showcasing collaborative problem-solving, clean UI design, and interactive JavaScript-driven gameplay.",
       skills: ["HTML", "CSS", "JavaScript"],
+      codeLink: "https://github.com/NatalieVinyu/Quiz",
+      demoLink: "https://quizprj.netlify.app/",
     },
     {
       image: project3,
       title: "Dictionary App",
-      description: "Collaborative task management platform with real-time update, team features, and advanced filtering capabilities",
+      description: "A simple and user-friendly dictionary application that allows users to search for word definitions in real time using an external API.",
       skills: ["HTML", "CSS", "JavaScript", "API"],
+      codeLink: "https://github.com/NatalieVinyu/Dictionary-App",
+      demoLink: "https://jocular-trifle-574e97.netlify.app/",
     },
   ]
 
   return (
     <div className='bg-fuchsia-50'>
       <section id='about' className='max-w-7xl mx-auto px-8 py-24'>
-        <div className='flex flex-col items-center '>
+        <div className='flex flex-col items-center pb-4 '>
           <h2 className='text-2xl'>Projects</h2>
-          <div className='w-20 h-1 bg-black mt-2 rounded-full'></div>
+          <div className='w-20 h-1 bg-fuchsia-300 rounded-full'></div>
         </div>
 
-        <div className='text-center p-10'>
-          <p>Here are some of my recent projects showcasing my skills and passion for web development</p>
+        <div className='text-center pb-10'>
+          <p className='text-gray-700'>Here are some of my recent projects showcasing my skills and passion for web development</p>
         </div>
 
         <div className='pt-6'>
           <div className='grid md:grid-cols-3 gap-4'>
             {myprojects.map((item, index) => (
-              <div className='border-2 border-fuchsia-300 rounded-xl p-8' key={index}>
-                <img src={item.image} alt={item.title} className='transition duration-700 ease-in-out transform hover:scale-105' />
+              <div className='border-2 border-fuchsia-300 rounded-xl p-8 shadow-lg' key={index}>
+                <img src={item.image} alt={item.title} className='transition duration-700 ease-in-out transform hover:scale-105 shadow-lg rounded-lg' />
 
                 <h3 className='text-2xl font-bold pt-4'>{item.title}</h3>
-                <p className='pt-4'>{item.description}</p>
+                <p className='pt-4 text-gray-700'>{item.description}</p>
 
                 <div className='pt-6'>
                   <ul className='flex gap-4'>
                   {item.skills.map((skill, i) => (
-                  <li className='rounded-xl bg-fuchsia-100 p-2' key={i}>{skill}</li>
+                  <li className='rounded-xl bg-fuchsia-100 p-2 text-sm text-gray-700' key={i}>{skill}</li>
                   ))}</ul>
                 </div>
 
                 <div className='flex gap-4 pt-8'>
-                  <button className='w-full flex items-center justify-center gap-2 border-2 border-fuchsia-300 rounded-xl py-2'>
+                  <a 
+                    href={item.codeLink} 
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='w-full flex items-center justify-center gap-2 border-2 border-fuchsia-300 rounded-xl py-2 transition duration-300 ease-in-out transform hover:scale-105 cursor-pointer'>
                     <Code />
                     <span>Code</span>
-                  </button>
-                  <button className='w-full flex items-center justify-center gap-2 border-2 border-fuchsia-300 rounded-xl py-2'>
+                  </a>
+                  <a 
+                    href={item.demoLink}
+                    target='_blank'
+                    rel='noopener noreferrer' 
+                    className='w-full flex items-center justify-center gap-2 border-2 border-fuchsia-300 bg-fuchsia-200 rounded-xl py-2 transition duration-300 ease-in-out transform hover:scale-105 cursor-pointer'>
                     <ExternalLink />
                     <span>Demo</span>
-                  </button>
+                  </a>
                 </div>
               </div>
             ))}
