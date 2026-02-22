249a217bf0092be9f43f84450ac38e3a0fa94f8a
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-02-22 12:17:01 +0200

    Navigation links transitions

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -1,32 +1,34 @@
 import React from 'react'
 
-
 function Navbar() {
   return (
      <div className='w-full bg-transparent backdrop-blur-md shadow-sm p-4 z-10 top-0'>
-      <nav className='container'>  
+      {/* NAVIGATION CONTAINER */}
+      <nav className='container'>
+
+          {/* NAVIGATION LINKS */}
           <ul className='flex space-x-14 justify-center'>
-            <li>
+            <li className='duration-500 transition-all hover:text-fuchsia-300 transform hover:scale-130'>
               <a href='#home'>
                 Home
               </a>
             </li>
-            <li>
+            <li className='duration-500 transition-all hover:text-fuchsia-300 transform hover:scale-130'>
               <a href='#about'>
                 About
               </a>
             </li>
-            <li>
+            <li className='duration-500 transition-all hover:text-fuchsia-300 transform hover:scale-130'>
               <a href='#skills'>
                 Skills
               </a>
             </li>
-            <li>
+            <li className='duration-500 transition-all hover:text-fuchsia-300 transform hover:scale-130'>
               <a href='#projects'>
                 Projects
               </a>
             </li>
-            <li>
+            <li className='duration-500 transition-all hover:text-fuchsia-300 transform hover:scale-130'>
               <a href='#contact'>
                 Contact
               </a>
