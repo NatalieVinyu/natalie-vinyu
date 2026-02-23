8c910eef2e6aa96b651c6f55ed46aa2495a427ad
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-02-23 20:53:35 +0200

    Improved styling and responsiveness

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,21 +17,25 @@ function Skills() {
   ]
 
   return (
-    <div className='dark:bg-black dark:text-white scroll-smooth '>
-      <section className='max-w-7xl mx-auto px-8 py-24'>
+    <div className='dark:bg-slate-950 dark:text-white scroll-smooth '>
+
+      {/* SKILLS SECTION */}
+      <section className='max-w-7xl max-w-sm mx-auto px-8 py-24'>
+
         <div className='flex flex-col items-center '>
-          <h2 className='text-3xl font-bold'>Skills and Technologies</h2>
-          <div className='w-50 h-1 mt-2 rounded-full bg-fuchsia-200'></div>
+          <h2 className='md:text-3xl sm:text-2xl font-bold'>Skills and Technologies</h2>
+          <div className='w-24 h-1 mt-2 rounded-full bg-fuchsia-200'></div>
         </div>
 
-       <div className='grid md:grid-cols-3'>
+      {/* SKILLS GRID */}
+       <div className='grid grid-cols-1 md:grid-cols-3 gap-6 sm:gap-4 mt-10'>
           {skills.map((item, index) => (
             <div className='border-2 border-fuchsia-300 rounded-xl m-10 p-2 shadow-lg' key={index}>
               <h3 className='text-2xl font-bold text-center pt-4'>{item.title}</h3>
 
-              <ul className='flex flex-wrap space-x-4 list-none justify-center p-4 dark:text-black '>
+              <ul className='flex flex-wrap justify-center p-4 dark:text-black'>
                 {item.skills.map((skill, i) => (
-                <div className='border border-fuchsia-300 bg-fuchsia-50 rounded-full m-2 p-3 transition duration-300 ease-in-out transform hover:scale-125 hover:border-fuchsia-400 hover:shadow-lg' key={i}>
+                <div className='border border-fuchsia-300 bg-fuchsia-50 rounded-full m-2 p-3 transition duration-300 ease-in-out transform hover:scale-125 hover:border-fuchsia-400 hover:shadow-lg sm:text-sm' key={i}>
                 <li key={i}>{skill}</li>
                 </div>
                 ))}
